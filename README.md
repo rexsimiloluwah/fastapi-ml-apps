897df4a2d9856cce6c6c390f44234c54b170f387
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-02-15 05:37:46 +0000

    Updated README.md

--- README.md
+++ README.md
@@ -5,6 +5,11 @@
 
 Just a collection of simple machine learning apps built and deployed using FastAPI and Python.
 
+- [Heart disease prediction app](https://fastapi-ml-apps.herokuapp.com/heartdisease)
+- [Sentiment analysis app (NLP)](https://fastapi-ml-apps.herokuapp.com/moviessentiment)
+- [Object detection app (Computer vision)](https://fastapi-ml-apps.herokuapp.com/object-detection)
+- Flower multi-classification app (Computer vision)
+
 ## Running the app 
 
 **1.** Running the app locally on a development server
@@ -39,6 +44,44 @@ $ docker pull similoluwaokunowo/fastapi-ml-apps
 
 *Computer vision app is not live yet due to slug size issues with tensorflow and keras 
 
+> All APIs are available on http://localhost:5000/docs locally or fastapi-ml-apps.herokuapp.com/docs
+
+i.e. For the Object detection API:- 
+
+**Sample request**
+
+<img src="sampleimages/dog-hug.jpg" alt="sample image" />
+
+```
+curl --location --request POST 'http://localhost:5000/objectdetection/predict' --form 'file=@"/C:/Users/IT/Downloads/dog-hug.jpg"'
+```
+
+**Response**
+```
+{
+    "status": true,
+    "data": {
+        "predictions": [
+            {
+                "class": "dog",
+                "confidence": 0.9255891442298889,
+                "bounding_box": [3, 386, 1098, 1340],
+                "color": [28, 220, 131]
+            },
+            {
+                "class": "person",
+                "confidence": 0.9850807785987854,
+                "bounding_box": [848, 146, 2002, 1330],
+                "color": [48, 84, 0]
+            }
+        ]
+    }
+}
+```
+
+<img src="sampleimages/objectdetectionoutput1.jpg" alt="response">
+
 To view the live app :- 
 > Go to https://fastapi-ml-apps.herokuapp.com
 
+Thanks for reading.
--- app/objectdetection/utils.py
+++ app/objectdetection/utils.py
@@ -21,6 +21,16 @@ MODELS_DIR = os.path.join(os.path.dirname(__file__), "models")
 OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "uploads")
 LABELS = os.path.join(os.path.dirname(__file__), "labels.json")
 
+# COLORS = np.random.uniform(0,255, size = (len(self.labels.values()), 3))
+
+#  For darker shades
+COLORS = [
+    (255,0,0),(81,14,14),(213,66,174),(87,1,64),(220,87,225),(62,18,238),
+    (18,91,238),(0,255,0),(0,0,255),(18,164,238),(18,223,238),(68,87,88),
+    (28,220,131),(28,220,79),(220,214,28),(48,84,0),(0,84,81),(23,41,53),
+    (213,140,30),(98,39,5),(13,255,14)
+]
+
 if not os.path.isdir(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
 
@@ -33,14 +43,34 @@ def read_image_file(file) -> Image.Image:
 class SSDObjectDetection:
     def __init__(self, save_image = False):
         self.weights_path = os.path.join(MODELS_DIR, "MobileNetSSD_deploy.caffemodel")
-        self.prototxt_path = os.path.join(MODELS_DIR, "MobileNet_deploy.prototxt")
+        self.prototxt_path = os.path.join(MODELS_DIR, "MobileNetSSD_deploy.prototxt")
         self.labels = json.load(open(LABELS))
-        self.colors = np.random.uniform(0,255, size = (len(self.labels.values()), 3))
+        self.colors = COLORS
         self.save_image = save_image
     
     def _load_model(self):
         # Using cv2 dnn module 
         self.net = cv2.dnn.readNetFromCaffe(self.prototxt_path, self.weights_path)
+
+    @staticmethod
+    #SOURCE :- https://stackoverflow.com/questions/60674501/how-to-make-black-background-in-cv2-puttext-with-python-opencv
+    def draw_text(
+          img,
+          text,
+          font=cv2.FONT_HERSHEY_PLAIN,
+          pos=(0, 0),
+          font_scale=3,
+          font_thickness=2,
+          text_color=(0, 0, 0),
+          text_color_bg=(0, 0, 0)
+        ):
+
+        x, y = pos
+        text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
+        text_w, text_h = text_size
+        cv2.rectangle(img, pos, (x + text_w + 20, y + text_h + 20), text_color_bg, -1)
+        cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+
     
     def _detect(self, image, display=False):
         image_copy = image.copy()
@@ -54,7 +84,7 @@ class SSDObjectDetection:
         # Construct an input blob for the network
         image_resized = cv2.resize(image, (300,300))
         print(image_resized.shape)
-        blob = cv2.dnn.blobFromImage(image_resized, 0.007843, (300,300), 127.5)
+        blob = cv2.dnn.blobFromImage(cv2.resize(image, (300,300)), 0.007843, (300,300), 127.5)
         self.net.setInput(blob)
         detections = self.net.forward()
         # print(detections.shape)
@@ -79,13 +109,15 @@ class SSDObjectDetection:
 
                 (startX, startY, endX, endY) = box.astype("int")
                 # Put a rectangle around the prediction's bounding box
-                cv2.rectangle(image, (startX, startY), (endX, endY), self.colors[idx], 5)
-
+                cv2.rectangle(image, (startX, startY), (endX, endY), self.colors[idx], 3)
+                textboxendx = int(endX*0.3)
+                # cv2.rectangle(image, (startX, startY), (startX+textboxendx, startY+60), self.colors[idx], cv2.FILLED)
                 # Add text label 
-                label = list(self.labels.values())[idx]
+                label = f"{list(self.labels.values())[idx]} {round(confidence*100,2)}%"
                 y = startY - 15 if startY - 15 > 15 else startY + 15
                 print(list(self.colors[idx]))
-                cv2.putText(image, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 2, self.colors[idx], 2)
+                # cv2.putText(image, label, (startX, startY + 35), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2)
+                self.draw_text(image,label,pos=(startX,startY+35),text_color_bg=self.colors[idx])
         
         if self.save_image:
             image_id = shortuuid.ShortUUID().random(length = 8)
